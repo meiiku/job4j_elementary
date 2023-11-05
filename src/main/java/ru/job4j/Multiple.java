646ac9326b706b3be5e1b4eadcfa2c9b2c06c276
fedor <f.b.fokin@gmail.com>
2023-11-05 23:38:10 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -3,7 +3,7 @@ package ru.job4j;
 public class Multiple {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("1 * " + i + " = " + i*1);
+            System.out.println("1 * " + i + " = " + i * 1);
         }
     }
 }
