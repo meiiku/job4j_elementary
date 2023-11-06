638822ddd285bb18de07b8b74f840dc7615d6721
fedor <f.b.fokin@gmail.com>
2023-11-06 13:46:19 +0300

    1. Модульные тесты. [#159435]

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,20 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static float rubleToEuro(float value) {
+        return value / 70;
+    }
+
+    public static float rubleToDollar(float value) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+    }
+}
